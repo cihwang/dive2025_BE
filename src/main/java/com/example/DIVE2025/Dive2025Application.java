7b947e202d4cc324cd0c023ebd91afe6bfa83c49
chanil <hci970705@gmail.com>
2025-08-05 22:47:00 +0900

    init

---
+++ src/main/java/com/example/DIVE2025/Dive2025Application.java
@@ -0,0 +1,13 @@
+package com.example.DIVE2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dive2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dive2025Application.class, args);
+	}
+
+}
