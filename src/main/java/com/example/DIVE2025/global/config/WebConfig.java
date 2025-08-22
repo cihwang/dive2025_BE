e28fe7b7b3a2ddf9bf9a671a84d120671ad39c80
chanil <hci970705@gmail.com>
2025-08-23 01:36:18 +0900

    feat: CORS 설정, transportMapper 수정

--- src/main/java/com/example/DIVE2025/global/config/WebConfig.java
+++ src/main/java/com/example/DIVE2025/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.DIVE2025.domain.security.config;
+package com.example.DIVE2025.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4321") // 프론트엔드 도메인
+                .allowedOrigins("http://localhost:5173", "https://main.d3tok4dzb6lic5.amplifyapp.com") // 프론트엔드 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
